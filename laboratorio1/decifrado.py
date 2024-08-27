6abe69a65f88eb901ef3b22aff6653075306b100
Benjamin Morales <benjamin.morales3@mail.udp.cl>
2024-08-27 13:48:26 -0400

    feat: imagenes

--- laboratorio1/decifrado.py
+++ laboratorio1/decifrado.py
@@ -1,6 +1,3 @@
-# DECIFRADO CESAR
-
-# se realiza el corrimiento del caracter
 def transform(character, movement):
     if 'a' <= character <= 'z':
         ascii_code = ord(character)
@@ -9,13 +6,22 @@ def transform(character, movement):
     else:
         return character
 
-# se realiza cada desplazamiento utilizando la funcion anterior
+def print_colored(text, color_code):
+    # Código de escape ANSI para el color
+    print(f"\033[{color_code}m{text}\033[0m")
+
 def decifrate_all(word):
+    target_message = "criptografia y seguridad en redes"
     for movement in range(26):
         result = ""
         for letter in word:
             result += transform(letter, -movement)
-        print(f"Desplazamiento de {movement}: {result}")
+        
+        # Verifica si el resultado es el mensaje objetivo
+        if result == target_message:
+            print_colored(f"Desplazamiento de {movement}: {result}", '32')  # Verde
+        else:
+            print(f"Desplazamiento de {movement}: {result}")
 
 # Se pasa el texto cifrado y se obtienen todas las combinaciones de desplazamientos.
 # cifrado = "larycxpajorj h bnpdarmjm nw anmnb"
